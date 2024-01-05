fe14c0eed2db6687ebd9e736f05af3754b31ef95
Abhijith <abhijithcm19@gmail.com>
2024-01-05 22:26:45 +0530

    old

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+//middleware
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/public", express.static("public"));
+app.use("/", require("./routes/bookRoutes"));
+
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`connected to PORT : ${PORT}`);
+});
